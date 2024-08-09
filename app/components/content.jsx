266df79e1736b06d22adbf8cb9c8f060d0a5b66c
RicardoDevValenzuela <rvalenzuela@disruptivestudio.com>
2024-08-09 10:40:24 -0700

    images and buttons

--- app/components/content.jsx
+++ app/components/content.jsx
@@ -1,8 +1,18 @@
 const Content = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Noticia</h1>
-      <button></button>
+    <div className="flex items-start">
+      <div className="flex flex-col items-center">
+        <h1 className="flex text-4xl">Lorem impson</h1>
+        <div>
+          <button className="px-20 py-4 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            Mas informacion
+          </button>
+        </div>
+      </div>
+      <img
+        src="images/placeholder.png"
+        style={{ width: "800px", height: "600px" }}
+      ></img>
     </div>
   );
 };
